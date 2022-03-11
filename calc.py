1c7c9629c99eff4b0ce25371cde88d68869ada93
MrMallik <mallik.pritabrata@gmail.com>
2022-03-12 02:47:09 +0530

    added multiply feature

--- calc.py
+++ calc.py
@@ -1,10 +1,12 @@
 from add import add
 from sub import sub
+from mul import mul
 
 num1 = 69
 num2 = 31
 
 sum = add(num1, num2)
 diff = sub(num1, num2)
+prod = mul(num1, num2)
 
-print(sum, diff)
+print(sum, diff, prod)
---
+++ mul.py
@@ -0,0 +1,7 @@
+def mul(*argv):
+    res = 1
+
+    for i in argv:
+        res *= i
+    
+    return res
